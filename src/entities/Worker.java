47de715fed0faccfaa313b8b8fa8c836ba8bd726
Paloma Falchi <palomapfalchi@gmail.com>
2021-12-06 11:20:15 -0300

    Initial commit

---
+++ src/entities/Worker.java
@@ -0,0 +1,87 @@
+package entities;
+
+import entities.enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Worker {
+
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public void income(int year, int month) {
+        //adiciona o salario base na soma
+        double sum = baseSalary;
+        //instancia o calendario com a data do contrato
+        Calendar calendar = Calendar.getInstance();
+        for (HourContract c : contracts) {
+            //seleciona a data do contrato e define como sendo a data do calendario
+            calendar.setTime(c.getDate());
+            int c_year = calendar.get(Calendar.YEAR);
+            int c_month = 1 + calendar.get(Calendar.MONTH);
+            if (year == c_year && month == c_month) {
+                sum += c.totalValue();
+            }
+        }
+    }
+}
